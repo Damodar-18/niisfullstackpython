d50eaac145774a0066e2c22236d1c9411e447e9d
Damodar mandal <damodarmandal543@gmail.com>
2026-03-22 09:56:37 +0530

    text

---
+++ python niis/list/tuple/tuple1.py
@@ -0,0 +1,8 @@
+t=28
+print(type(t))
+s=10,"hii",2.5
+for i in s:
+	print(i)
+for i in range(len(s)):
+	print(s[i])
+	print(s[1])
---
+++ python niis/list/tuple/tuple3.py
@@ -0,0 +1,3 @@
+t=(10,"hii",4.5,10,"bye")
+print(t.count(10))    #count
+print(t.index("bye"))   #index
