02cbac7fe87bfd09747b0e19fbc6c49ee3e4ad65
Damodar mandal <damodarmandal543@gmail.com>
2026-04-06 06:08:57 +0530

    text

--- python niis/parton program/hw11.py
+++ python niis/parton program/hw11.py
@@ -2,3 +2,8 @@ for i in range(1,5,1):
 	for j in range(5-i,5,1):
 		print(chr(64+j),end="\t")
 	print()
+
+for i in range(68,64,-1):      #new one
+	for j in range(i,69,1):
+		print(chr(j),end="\t")  
+	print()
